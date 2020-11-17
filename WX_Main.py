ca963f463088db9d3e28d757abeaed62dd63e2b5
Shinomiya <mikufans@nicomoe.cn>
2020-11-17 12:31:47 +0800

    add card

--- WX_Main.py
+++ WX_Main.py
@@ -1,5 +1,6 @@
 import werobot
 import re
+import requests
 
 robot = werobot.WeRoBot(token='hello')
 
@@ -10,12 +11,20 @@ def help(message):
     return reply
 
 
-def get_username(message):
-    pass
+@robot.filter(re.compile(".*?洛天依借记卡.*?"))
+def card(message):
+    response = requests.post(
+        url='https://cloud.bankofchina.com/sh/api/cas_SC/common/jjk/queryJJKCardProgress/esb',
+        data={
+            'name': '王蕾锦',
+            'idType': '01',
+            'id': '510524199907250817'
+        })
+    return response.json()
 
 
 robot.config['HOST'] = '0.0.0.0'
-robot.config['PORT'] = 10000
+robot.config['PORT'] = 80
 robot.config["APP_ID"] = "wx395b40f4589090ce"
 robot.config['ENCODING_AES_KEY'] = 'yWNNztIzU9QCzi8Fu1B74Xe2prp3kpkX2aGqNdUJha4'
 robot.run()
